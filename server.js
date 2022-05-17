ac2a0b54226ceba208b4389ca623466bac9075e7
Alexandre Yudi <alexyud@hotmail.com>
2022-05-16 22:52:49 -0300

    Arrumando bug na url angular

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-app.use(express.static('www/index.html'));
+//app.use(express.static(__dirname + '/www'));
 app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
@@ -8,6 +9,12 @@ app.all('*', function(req, res, next) {
 });
 
 app.set('port', process.env.PORT || 5000);
+
+app_path ='/www';
+
+app.use('/',express.static(path.join(__dirname,app_path)))
+
+app.get('*',(req,res)=>res.sendFile(path.join(__dirname,app_path + '/index.html')))
 
 app.listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
