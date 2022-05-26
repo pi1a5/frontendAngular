b432c5f40c9185d72a8d689f4be29a4e5d65ac5d
Alexandre Yudi <61790578+AlexYud@users.noreply.github.com>
2022-05-26 13:26:15 -0300

    Delete src/environments directory

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  bucket: 'pi1a5',
-  accessKeyId: 'AKIA4VWXXQ5BD45EOVPK',
-  secretAccessKey: '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',
-  region: 'sa-east-1'
-};
