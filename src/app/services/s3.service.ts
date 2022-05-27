04bbce31c046bbd64d4d43b5c2176e42a2f566c6
Alexandre Yudi <alexyud@hotmail.com>
2022-05-27 12:14:25 -0300

    Testando deploy

--- src/app/services/s3.service.ts
+++ src/app/services/s3.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 //import * as AWS from 'aws-sdk/global';
 import * as S3 from 'aws-sdk/clients/s3';
-import { environment } from './../../environments/environment';
+import { environment } from './../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
