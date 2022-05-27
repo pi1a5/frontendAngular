c1e0e8b0225259279ac38f1c92ae7e7555ac9ce4
Alexandre Yudi <alexyud@hotmail.com>
2022-05-27 12:53:11 -0300

    Testando deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
   bucket: 'pi1a5',
-  accessKeyId: process.env.ACCESS_KEY_ID,
+  //accessKeyId: process.env.ACCESS_KEY_ID,
+  accessKeyId: 'path/to/prod/env',
   secretAccessKey: process.env.SECRET_ACCESS_KEY,
   region: 'sa-east-1'
 };
