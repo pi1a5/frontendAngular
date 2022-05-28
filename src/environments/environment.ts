a101f92eda3615ac8a3fae685d557dbf4e68c6bc
Alexandre Yudi <alexyud@hotmail.com>
2022-05-28 01:17:54 -0300

    Heroku

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,4 @@
 
   export const environment = {
     production: false,
-    BUCKET: 'pi1a5',
-    ACCESS_KEY_ID: 'AKIA4VWXXQ5BBSW7RR2V',
-    SECRET_ACCESS_KEY: '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,',
-    REGION: 'sa-east-1'
   };
