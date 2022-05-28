d83fc772ad8100d8de37af71f91d95fed12ec4d4
Alexandre Yudi <alexyud@hotmail.com>
2022-05-28 01:17:54 -0300

    Heroku

--- src/app/pages/acompanhamento/acompanhamento.page.ts
+++ src/app/pages/acompanhamento/acompanhamento.page.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoadingController, ToastController } from '@ionic/angular';
 import { ApiStudentService } from 'src/app/services/api-student.service';
-import { S3Service } from 'src/app/services/s3.service';
 
 @Component({
   selector: 'app-acompanhamento',
@@ -17,7 +16,6 @@ export class AcompanhamentoPage implements OnInit {
   constructor(
     public router: Router,
     public apiStudent: ApiStudentService,
-    public s3: S3Service,
     public toastController: ToastController,
     public loadingController: LoadingController
   ) { }
@@ -59,12 +57,17 @@ export class AcompanhamentoPage implements OnInit {
   async submit() {
     if (this.validate()) {
       await this.presentLoading();
-      var resp = await this.uploadS3();
-      if(resp == false) {
-        console.log('error');
-      } else {
-        this.uploadApi(resp);
-      }
+      this.apiStudent.sendTicketAcompanhamento(this.textArea, this.calculateDataLimite(), this.arqRAE).subscribe(data => {
+        console.log(data);
+        this.loadingController.dismiss();
+        this.presentToast(data, 'success', 'checkmark-circle');
+        this.router.navigate(['student'], { replaceUrl: true });
+      }, error => {
+        console.log(error);
+        this.loadingController.dismiss();
+        this.presentToast(error.error, 'danger', 'close-circle');
+        this.router.navigate(['student'], { replaceUrl: true });
+      });    
     }
     return;
   }
@@ -88,34 +91,6 @@ export class AcompanhamentoPage implements OnInit {
     } else {
       console.log('There is no file');
     }
-  }
-
-  async uploadS3() {
-    // s3
-    var key = await this.s3.uploadFile(this.arqRAE)
-
-    if (key == false) {
-      return false;
-    } else {
-      return [key];
-    }
-  }
-
-  async uploadApi(resp: any) {
-    // api 
-    var doc = `https://s3-sa-east-1.amazonaws.com/pi1a5/${resp[0]}`
-    this.apiStudent.sendTicketAcompanhamento(this.textArea, this.calculateDataLimite(), doc).subscribe(data => {
-      console.log(data);
-      this.loadingController.dismiss();
-      this.presentToast(data, 'success', 'checkmark-circle');
-      this.router.navigate(['student'], { replaceUrl: true });
-    }, error => {
-      console.log(error);
-      this.loadingController.dismiss();
-      this.presentToast(error.error, 'danger', 'close-circle');
-      this.router.navigate(['student'], { replaceUrl: true });
-    });
-
   }
 
 }
--- src/app/pages/inicio-de-estagio/inicio-de-estagio.page.ts
+++ src/app/pages/inicio-de-estagio/inicio-de-estagio.page.ts
@@ -3,7 +3,6 @@ import { Router } from '@angular/router';
 import { LoadingController, ToastController } from '@ionic/angular';
 import { format, parseISO } from 'date-fns';
 import { ApiStudentService } from 'src/app/services/api-student.service';
-import { S3Service } from 'src/app/services/s3.service';
 
 @Component({
   selector: 'app-inicio-de-estagio',
@@ -22,7 +21,6 @@ export class InicioDeEstagioPage implements OnInit {
   constructor(
     public router: Router,
     public apiStudent: ApiStudentService,
-    public s3: S3Service,
     public toastController: ToastController,
     public loadingController: LoadingController
   ) { }
@@ -56,12 +54,17 @@ export class InicioDeEstagioPage implements OnInit {
   async submit() {
     if (this.validate()) {
       await this.presentLoading();
-      var resp = await this.uploadS3();
-      if(resp == false) {
-        console.log('error');
-      } else {
-        this.uploadApi(resp);
-      }
+      this.apiStudent.sendTicketInicio(this.textArea, this.dateValue, this.arqTCE, this.arqPA).subscribe(data => {
+        console.log(data);
+        this.loadingController.dismiss();
+        this.presentToast(data, 'success', 'checkmark-circle');
+        this.router.navigate(['student'], { replaceUrl: true });
+      }, error => {
+        console.log(error);
+        this.loadingController.dismiss();
+        this.presentToast(error.error, 'danger', 'close-circle');
+        this.router.navigate(['student'], { replaceUrl: true });
+      })     
     }
     return;
   }
@@ -101,36 +104,6 @@ export class InicioDeEstagioPage implements OnInit {
     } else {
       console.log('There is no file');
     }
-  }
-
-  async uploadS3() {
-    // s3
-    var key1 = await this.s3.uploadFile(this.arqTCE)
-    var key2 = await this.s3.uploadFile(this.arqPA)
-
-    if (key1 == false || key2 == false) {
-      return false;
-    } else {
-      return [key1, key2];
-    }
-  }
-
-  async uploadApi(resp: any) {
-    // api 
-    var doc1 = `https://s3-sa-east-1.amazonaws.com/pi1a5/${resp[0]}`
-    var doc2 = `https://s3-sa-east-1.amazonaws.com/pi1a5/${resp[1]}`
-    this.apiStudent.sendTicketInicio(this.textArea, this.dateValue, doc1, doc2).subscribe(data => {
-      console.log(data);
-      this.loadingController.dismiss();
-      this.presentToast(data, 'success', 'checkmark-circle');
-      this.router.navigate(['student'], { replaceUrl: true });
-    }, error => {
-      console.log(error);
-      this.loadingController.dismiss();
-      this.presentToast(error.error, 'danger', 'close-circle');
-      this.router.navigate(['student'], { replaceUrl: true });
-    })
-
   }
 
 }
