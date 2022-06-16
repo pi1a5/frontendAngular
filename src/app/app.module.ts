f02fa269106a4fe1aebe4d0e595dcb5d5d53cf41
Alexandre Yudi <alexyud@hotmail.com>
2022-06-16 19:45:30 -0300

    Adicionando security headers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,23 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { InterceptorService } from './provider/interceptor.service';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, HttpClientModule, IonicModule.forRoot(), AppRoutingModule, Ng2SearchPipeModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    {
+      provide: RouteReuseStrategy,
+      useClass: IonicRouteStrategy
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: InterceptorService,
+      multi: true,
+    }],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
---
+++ src/app/provider/interceptor.service.ts
@@ -0,0 +1,26 @@
+import { HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InterceptorService implements HttpInterceptor {
+
+  constructor() { }
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    // Clone the request to add the new header
+    
+    const clonedRequest = req.clone({ headers: new HttpHeaders({
+      'Strict-Transport-Security': 'max-age=31536000; includeSubDomains',
+      'Content-Security-Policy': "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self'",
+      'X-Frame-Options': 'SAMEORIGIN',
+      'X-Content-Type-Options': 'nosniff',
+      'Referrer-Policy': 'no-referrer',
+      'Permissions-Policy': 'geolocation=(self "https://pi1a5frontend-angular.herokuapp.com")'
+    }) });
+
+    // Pass the cloned request instead of the original request to the next handle
+    return next.handle(clonedRequest);
+  }
+}
