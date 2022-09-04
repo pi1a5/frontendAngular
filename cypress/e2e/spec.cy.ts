69004a4d052052136a21fff0ccc400d02f732f86
Deivid Almeida <deividalmeida365@gmail.com>
2022-09-03 21:34:02 -0300

    novas mudancas

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -2,3 +2,8 @@ it('loads examples', () => {
   cy.visit('/supervisor');
   cy.contains('Gerenciamento de tickets');
 });
+
+it('loads examples', () => {
+  cy.visit('/home');
+  cy.contains('Gerenciamento de tickets');
+});
