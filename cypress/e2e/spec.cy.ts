1a84e417ef6cc7d18639fc68cbdf83365416d9c4
Deivid Almeida <deividalmeida365@gmail.com>
2022-09-04 03:35:25 -0300

    e2e

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,9 +1,29 @@
-it('loads examples', () => {
-  cy.visit('/supervisor');
-  cy.contains('Gerenciamento de tickets');
+describe('Testes Sistema de Estágio IFSP', () => {
+
+  
+  // testar a página principal da aplicação //
+  it('loads examples', () => {
+    cy.visit('/home');
+    cy.contains('Nunca foi tão fácil gerenciar processos de estágio');
+  });
+  
+  it('loads examples', () => {
+    cy.visit('/student');
+    cy.contains('Gerenciamento de tickets');
+  });
+  
+  it('loads examples', () => {
+    cy.visit('/home');
+    cy.contains('Gerenciamento de tickets');
+  });
+  
+  it('loads examples', () => {
+    cy.visit('/select-course');
+    cy.contains('Gerenciamento de tickets');
+    cy.check('.')
+  });
+
+
 });
 
-it('loads examples', () => {
-  cy.visit('/home');
-  cy.contains('Gerenciamento de tickets');
-});
+
