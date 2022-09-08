5245d0d7d84d773eb5d3df229c6cc96a1cd6f2e0
Deivid Almeida <deividalmeida365@gmail.com>
2022-09-08 00:18:52 -0300

    cypress tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,4 +1,16 @@
-it('loads examples', () => {
-  cy.visit('/supervisor');
-  cy.contains('Gerenciamento de tickets');
+
+describe('Realizar o Login com conta de Estudante e Professor', () => {
+
+  it('realiza login estudante', () => {
+    cy.visit('/supervisor');
+    cy.contains('Gerenciamento de tickets');
+  });
+  
+  
+  it('realiza login professor', () => {
+      cy.visit('/supervisor');
+    cy.contains('Gerenciamento de tickets');
+  });
+  
 });
+
