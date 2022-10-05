5b1d73fcf3a65be3de0c64fddc900ef3eb3b7de5
Deivid Almeida <deividalmeida365@gmail.com>
2022-10-04 21:18:26 -0300

    escolherCursosTests

--- cypress/e2e/EfetuarLogin.cy.ts
+++ cypress/e2e/EfetuarLogin.cy.ts
@@ -1,38 +1,25 @@
 describe('Acessar Rota para Efetuar Login', () => {
 
+  // https://pi1a5frontend-angular.herokuapp.com/home
+  // https://accounts.google.com/o/oauth2/auth?redirect_uri=storagerelay%3A%2F%2Fhttps%2Fpi1a5frontend-angular.herokuapp.com%3Fid%3Dauth964337&response_type=permission%20id_token&scope=email%20profile%20openid&openid.realm=&include_granted_scopes=true&client_id=376654667965-lq5rc8l7q8c6cdtl2gdc6baj6d15n3bs.apps.googleusercontent.com&ss_domain=https%3A%2F%2Fpi1a5frontend-angular.herokuapp.com&fetch_basic_profile=true&gsiwebsdk=2
   beforeEach(() => {
-    cy.visit('https://accounts.google.com/o/oauth2/auth?redirect_uri=storagerelay%3A%2F%2Fhttps%2Fpi1a5frontend-angular.herokuapp.com%3Fid%3Dauth964337&response_type=permission%20id_token&scope=email%20profile%20openid&openid.realm=&include_granted_scopes=true&client_id=376654667965-lq5rc8l7q8c6cdtl2gdc6baj6d15n3bs.apps.googleusercontent.com&ss_domain=https%3A%2F%2Fpi1a5frontend-angular.herokuapp.com&fetch_basic_profile=true&gsiwebsdk=2');
+    cy.visit('https://pi1a5frontend-angular.herokuapp.com/home');
   });
 
 
-  it('testar botão login', () => {
-    cy.get('.cxMOTc > .rFrNMe').type('deivid.almeida@aluno.ifsp.edu.br');
-    cy.get('#identifierNext > .VfPpkd-dgl2Hf-ppHlrf-sM5MNb > .VfPpkd-LgbsSe > .VfPpkd-vQzf8d').click();
-    cy.get('#password > .aCsJod > .aXBtI > .OabDMe');  
+  it('Acessar Botão de Login', () => {
+
+    cy.get('.font').click();
 
   });
 
+
+it('Acessar Curso Tecnico Integrado', () => {
+  cy.get('[value="tecnicoIntegrado"] > .item-has-start-slot > .ion-accordion-toggle-icon').click();
+
+})
 
 
 });
 
 
-/*
-describe('Acessar Propriedades rota home', () => {
-
-  beforeEach(() => {
-    cy.visit('/home');
-  });
-
-
-  it('testar botão login', () => {
-    cy.contains('teste');
-  });
-
-*/
-
-
-
-
-//https://accounts.google.com/o/oauth2/auth?redirect_uri=storagerelay%3A%2F%2Fhttps%2Fpi1a5frontend-angular.herokuapp.com%3Fid%3Dauth964337&response_type=permission%20id_token&scope=email%20profile%20openid&openid.realm=&include_granted_scopes=true&client_id=376654667965-lq5rc8l7q8c6cdtl2gdc6baj6d15n3bs.apps.googleusercontent.com&ss_domain=https%3A%2F%2Fpi1a5frontend-angular.herokuapp.com&fetch_basic_profile=true&gsiwebsdk=2
-  
--- cypress/e2e/EscolherCurso.cy.ts
+++ cypress/e2e/EscolherCurso.cy.ts
@@ -1,16 +1,107 @@
-describe('Escolher Curso Desejado', () => {
+describe('TESTE01: SELECIONAR CURSO DESEJADO', () => {
+
+  beforeEach(() => {
+    cy.visit('https://pi1a5frontend-angular.herokuapp.com/select-course');
+  });
 
 
-    beforeEach(() => {
-        cy.visit('/student');
-      });
 
-    it('Selecionar Nome do Curso Desejado', () => {
+    it('Selecionar nome do Curso desejado', () => {
+    cy.get('[value="tecnicoIntegrado"] > .item-has-start-slot > .ion-accordion-toggle-icon').click();
+    cy.get('.accordion-expanded > :nth-child(2) > .courseItem > .font').click();
 
-        cy.get('.ion-text-center > .button').click();
-        cy.contains('Técnico Integrado');
-      });
+  })
+
+/*
+  it('Prosseguir para próxima tela sem inserir prontuário', () => {
+    cy.get('[value="tecnicoIntegrado"] > .item-has-start-slot > .ion-accordion-toggle-icon').click();
+    cy.get('.accordion-expanded > :nth-child(2) > .courseItem > .font').click();
+
+    cy.get('.ion-padding-start').click();
+
+    cy.get('.native-input').type('SP31');
+
+  })
+  */
+});
+
+describe('TESTE02: PROSSEGUIR PARA A PRÓXIMA TELA SEM PRONTUÁRIO', () => {
+
+  beforeEach(() => {
+    cy.visit('https://pi1a5frontend-angular.herokuapp.com/select-course');
+  });
 
 
-})
+  it('Prosseguir para próxima tela sem inserir prontuário', () => {
+    cy.get('[value="tecnicoIntegrado"] > .item-has-start-slot > .ion-accordion-toggle-icon').click();
+    cy.get('.accordion-expanded > :nth-child(2) > .courseItem > .font').click();
 
+    cy.get('.ion-padding-start').click();
+
+    cy.get('.native-input').type('SP310');
+
+  })
+
+});
+
+
+describe('TESTE03: PROSSEGUIR PARA A PRÓXIMA TELA SEM SP PRONTUÁRIO', () => {
+
+  beforeEach(() => {
+    cy.visit('https://pi1a5frontend-angular.herokuapp.com/select-course');
+  });
+
+
+  it('Prosseguir para próxima tela sem inserir prontuário', () => {
+    cy.get('[value="tecnicoIntegrado"] > .item-has-start-slot > .ion-accordion-toggle-icon').click();
+    cy.get('.accordion-expanded > :nth-child(2) > .courseItem > .font').click();
+
+
+    cy.get('.native-input').type('3058123');
+    cy.get('.ion-padding-start').click();
+
+  })
+
+});
+
+
+describe('TESTE04: PROSSEGUIR PARA A PRÓXIMA COM MENOS DE 9 CARACTERES', () => {
+
+  beforeEach(() => {
+    cy.visit('https://pi1a5frontend-angular.herokuapp.com/select-course');
+  });
+
+
+  it('Prosseguir para próxima tela sem inserir prontuário', () => {
+    cy.get('[value="tecnicoIntegrado"] > .item-has-start-slot > .ion-accordion-toggle-icon').click();
+    cy.get('.accordion-expanded > :nth-child(2) > .courseItem > .font').click();
+
+
+    cy.get('.native-input').type('3058155');
+    cy.get('.ion-padding-start').click();
+
+    cy.contains("Mensagem de Erro")
+
+  });
+
+});
+
+
+describe('TESTE05: PROSSEGUIR PARA A PRÓXIMA COM PRONTUÁRIO CORRETO', () => {
+
+  beforeEach(() => {
+    cy.visit('https://pi1a5frontend-angular.herokuapp.com/select-course');
+  });
+
+
+  it('Prosseguir para próxima tela sem inserir prontuário', () => {
+    cy.get('[value="tecnicoIntegrado"] > .item-has-start-slot > .ion-accordion-toggle-icon').click();
+    cy.get('.accordion-expanded > :nth-child(2) > .courseItem > .font').click();
+
+
+    cy.get('.native-input').type('SP305313X');
+    cy.get('.ion-padding-start').click();
+
+  });
+
+});
