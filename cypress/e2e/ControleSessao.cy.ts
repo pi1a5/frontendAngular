bbbdd10da651e8c946ef2fe45adc6971a4fe92d1
Deivid Almeida <deividalmeida365@gmail.com>
2022-10-09 21:04:46 -0300

    sessionControl-02

--- cypress/e2e/ControleSessao.cy.ts
+++ cypress/e2e/ControleSessao.cy.ts
@@ -20,15 +20,19 @@ describe('TESTE 02: Controle de Sessão de rotas', () => {
 
 
   beforeEach(() => {
-    cy.visit('localhost:4200/home')
+    cy.visit('https://pi1a5frontend-angular.herokuapp.com/student')
 
     
   });
 
-    it('Acessar uma rota sem estar logado (Escolher Curso)', () => {
+    it('Acessar uma rota sem estando logado (Dashboard Estudante)', () => {
       cy.get('.title')
-      cy.get('.accordion-expanded > :nth-child(2) > .courseItem > .font').click();
-    })
+      cy.get('.ion-text-start > .ion-inherit-color')    
+    
+      cy.get('.contentHeight')
+      cy.get('.emptyTicketIcon')
+      cy.get('[size-xl="3"] > .ion-color-light > .ion-color > .md')
+      })
 
 
 
