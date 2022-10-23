f2d8b7a3e51922e0a069cedd1d39ac94e67b60bc
Deivid Almeida <deividalmeida365@gmail.com>
2022-10-23 09:51:15 -0300

    cypress tests

---
+++ cypress/e2e/criarProcesso.cy.ts
@@ -0,0 +1,14 @@
+describe('[Teste 01]Clicar em Criar Processo', () => {
+
+
+
+    beforeEach(() => {
+        cy.visit('https://pi1a5frontend-angular.herokuapp.com/home');
+      });
+
+      it('Espera a criação de processo', () => {
+ 
+        
+
+      });
+})
