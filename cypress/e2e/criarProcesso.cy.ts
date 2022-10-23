eea11a45f923d9f0db29d72b0cb93e83d7f7673b
Deivid Almeida <deividalmeida365@gmail.com>
2022-10-23 17:53:32 -0300

    changes

--- cypress/e2e/criarProcesso.cy.ts
+++ cypress/e2e/criarProcesso.cy.ts
@@ -3,7 +3,7 @@ describe('[Teste 01]Clicar em Criar Processo', () => {
 
 
     beforeEach(() => {
-        cy.visit('https://pi1a5frontend-angular.herokuapp.com/home');
+        cy.visit('https://accounts.google.com/o/oauth2/auth/oauthchooseaccount?redirect_uri=storagerelay%3A%2F%2Fhttps%2Fpi1a5frontend-angular.herokuapp.com%3Fid%3Dauth15659&response_type=permission%20id_token&scope=email%20profile%20openid&openid.realm&include_granted_scopes=true&client_id=376654667965-lq5rc8l7q8c6cdtl2gdc6baj6d15n3bs.apps.googleusercontent.com&ss_domain=https%3A%2F%2Fpi1a5frontend-angular.herokuapp.com&fetch_basic_profile=true&gsiwebsdk=2&flowName=GeneralOAuthFlow');
       });
 
       it('Espera a criação de processo', () => {
